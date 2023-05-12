b2336e993eb8a61dce9371a960a09a2edab66416
german-ipesriera <german.ipesriera@ukg.com>
2023-05-12 13:05:31 -0300

    cors enabled

--- src/main/kotlin/modules/HttpServer.kt
+++ src/main/kotlin/modules/HttpServer.kt
@@ -5,8 +5,14 @@ import io.javalin.Javalin
 object HttpServer {
     const val PORT = 8080
 
-    fun start(controllerModule: ControllerModule){
-        Javalin.create().routes {
+    fun start(controllerModule: ControllerModule) {
+        Javalin.create { config ->
+            config.plugins.enableCors { cors ->
+                cors.add {
+                    it.anyHost()
+                }
+            }
+        }.routes {
             controllerModule.gameController.routes()
         }.start(PORT)
     }
